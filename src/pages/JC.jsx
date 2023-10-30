d0fa0e1f0125a7745345ac4486fbdb829b5423f5
Gabriel Charrua <gabrielpqcharrua@gmail.com>
2023-10-30 20:59:44 +0000

    Update JC.jsx

--- src/pages/JC.jsx
+++ src/pages/JC.jsx
@@ -21,7 +21,7 @@ const JC = () => {
       }
 
     const fetchGameIds = () => {
-        fetch('https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/f1LMtURYzAjkYl5L8cv_s6cDVYlD39dQhXCj5JGzhTA2AB8UFwQ3VW9IIslj-MQSnZ2wMyD2f9PMTw/ids?start=0&count=40&api_key=RGAPI-d07add7a-f48b-4b1c-9358-cc59e01f6a29')
+        fetch('')
             .then(response => response.json())
             .then(json => {
                 setGameIds(json);
@@ -30,7 +30,7 @@ const JC = () => {
     }
 
     const fetchGame = (game) => {
-         fetch(`https://europe.api.riotgames.com/tft/match/v1/matches/${game}?api_key=RGAPI-d07add7a-f48b-4b1c-9358-cc59e01f6a29`)
+         fetch(`https://europe.api.riotgames.com/tft/match/v1/matches/${game}?api_key=`)
             .then(response => response.json())
             .then(json => {setGames(games =>[...games, json]); console.log(json)})
             .catch(error => console.error(error));
